6865cd9d96ed5dc468b3c71b1d2ad03548d9a545
Seongmin Park <llov0708@gmail.com>
2024-10-30 14:33:53 +0900

    Fix memory leak

--- src/jamo.zig
+++ src/jamo.zig
@@ -1,20 +1,35 @@
 const std = @import("std");
-const builtin = @import("builtin");
-
-//const expect = std.testing.expect;
 
 const SyllablePosition = enum(u8) { CODA, NUCLEUS, ONSET, NOT_APPLICABLE };
 
-const DisassembleResult = extern struct {
+const DisassembleResult = struct {
+    length: usize,
     is_hanguls: [*]bool,
-    is_hanguls_len: u64,
     jamos: [*][*]u8,
-    jamos_len: u64,
     codepoint_lengths: [*]u3,
     positions: [*]SyllablePosition,
-    positions_len: u64,
-    reserved_bytes: u64,
+    reserved_bytes: usize,
 };
+
+export fn cleanup(result: *DisassembleResult) void {
+    _cleanup(std.heap.page_allocator, result);
+}
+
+pub fn _cleanup(allocator: std.mem.Allocator, result: *DisassembleResult) void {
+    //const positions: std.ArrayList(SyllablePosition) = std.ArrayList.fromOwnedSlice(result.positions);
+    allocator.free(result.is_hanguls[0..result.length]);
+    const jamos = result.jamos[0..result.length];
+    for (0..result.length) |i| {
+        //for (0..result.codepoint_lengths[i]) |j| {
+        const length = result.codepoint_lengths[i];
+        const codepoints: []u8 = jamos[i][0..length];
+        allocator.free(codepoints);
+    }
+    allocator.free(result.jamos[0..result.length]);
+    allocator.free(result.codepoint_lengths[0..result.length]);
+    allocator.free(result.positions[0..result.length]);
+    //allocator.destroy(result);
+}
 
 const chosungs: [19]u21 = .{
     'ㄱ',
@@ -109,6 +124,7 @@ export fn disassemble(codepoints: [*:0]const u8) *DisassembleResult {
 
 pub fn _disassemble(allocator: std.mem.Allocator, codepoints: [*:0]const u8) *DisassembleResult {
     var unicode_codepoints = (std.unicode.Utf8View.init(std.mem.span(codepoints)) catch unreachable).iterator();
+
     const result = std.heap.page_allocator.create(DisassembleResult) catch unreachable;
 
     var is_hanguls = std.ArrayList(bool).init(allocator);
@@ -117,7 +133,6 @@ pub fn _disassemble(allocator: std.mem.Allocator, codepoints: [*:0]const u8) *Di
     var positions = std.ArrayList(SyllablePosition).init(allocator);
 
     var i: u32 = 0;
-    var ri: u32 = 0;
     while (unicode_codepoints.nextCodepoint()) |c| : (i += 1) {
         if (isWhitespace(c)) {
             continue;
@@ -129,29 +144,23 @@ pub fn _disassemble(allocator: std.mem.Allocator, codepoints: [*:0]const u8) *Di
             const jongsungCode = jongsungs[@mod(@mod(c - 0xAC00, (28 * 21)), 28)];
 
             collect(allocator, &is_hanguls, &jamos, &codepoint_lengths, &positions, true, chosungCode, SyllablePosition.ONSET);
-            ri += 1;
 
             collect(allocator, &is_hanguls, &jamos, &codepoint_lengths, &positions, true, joongsungCode, SyllablePosition.NUCLEUS);
-            ri += 1;
 
             if (jongsungCode != 'N') {
                 collect(allocator, &is_hanguls, &jamos, &codepoint_lengths, &positions, true, jongsungCode, SyllablePosition.CODA);
-                ri += 1;
             }
         } else {
             collect(allocator, &is_hanguls, &jamos, &codepoint_lengths, &positions, false, c, SyllablePosition.NOT_APPLICABLE);
         }
     }
-    result.is_hanguls = is_hanguls.items.ptr;
-    result.is_hanguls_len = is_hanguls.items.len;
-    result.jamos = jamos.items.ptr;
-    result.jamos_len = jamos.items.len;
-    result.codepoint_lengths = codepoint_lengths.items.ptr;
-    result.positions = positions.items.ptr;
-    result.positions_len = positions.items.len;
+    result.length = is_hanguls.items.len;
+    result.is_hanguls = (is_hanguls.toOwnedSlice() catch unreachable).ptr;
+    result.jamos = (jamos.toOwnedSlice() catch unreachable).ptr;
+    result.codepoint_lengths = (codepoint_lengths.toOwnedSlice() catch unreachable).ptr;
+    result.positions = (positions.toOwnedSlice() catch unreachable).ptr;
     return result;
 }
-
 pub fn isWhitespace(c: u21) bool {
     return for (std.ascii.whitespace) |other| {
         if (c == other)
@@ -164,10 +173,15 @@ test "testing disassemble with whitespace" {
     const allocator = std.testing.allocator;
     const test_string = "주 4일제는 포기 못합니다. We'll make it work.";
     const result = _disassemble(allocator, test_string);
-    std.debug.print("Jamos len {}\n", .{result.jamos_len});
-    for (0..result.jamos_len) |index| {
+    inline for (std.meta.fields(@TypeOf(result.*))) |field| {
+        _ = field.name;
+        //std.debug.print("Byte offset in bytes of {}: {}\n", .{ field.name, @offsetOf(result.*, field_name) });
+    }
+    std.debug.print("Jamos len {}\n", .{result.length});
+    for (0..result.length) |index| {
         const utf8_size = result.codepoint_lengths[index];
         const jamo = result.jamos[index][0..utf8_size];
         std.debug.print("Element {}: {s}\n", .{ index, jamo });
     }
+    _cleanup(allocator, result);
 }
