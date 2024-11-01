e1952500a129477e38227b01b1c78135f165a6b0
Seongmin Park <llov0708@gmail.com>
2024-11-02 00:18:59 +0900

    Target both

--- typescript/src/jamo.ts
+++ typescript/src/jamo.ts
@@ -1,11 +1,12 @@
-const LENGTH_OFFSET: number = 0;
-const IS_HANGULS_OFFSET: number = 8;
-const JAMOS_OFFSET: number = 16;
-const CODEPOINT_LENGTHS_OFFSET: number = 24;
-const POSITIONS_OFFSET: number = 32;
-const nullByte = 0x00;
+import { createRequire } from "module";
+//const LENGTH_OFFSET: number = 0;
+//const IS_HANGULS_OFFSET: number = 8;
+//const JAMOS_OFFSET: number = 16;
+//const CODEPOINT_LENGTHS_OFFSET: number = 24;
+//const POSITIONS_OFFSET: number = 32;
+//const nullByte = 0x00;
 
-import wasmUrl from "./hama.wasm";
+//import wasm from '/home/seongmin/hama/zig-out/bin/hama.wasm';
 
 //const decodeNullTerminatedString = (pointer: number) => {
 //	const slice = new Uint8Array(memory.buffer, pointer);
@@ -30,54 +31,69 @@ let importObject = {
 };
 
 class JamoParser {
-  private wasmModule: WebAssembly.Module | null = null;
+  //private wasmModule: WebAssembly.Module | null = null;
   private wasmInstance: WebAssembly.Instance | null = null;
   private memory: WebAssembly.Memory | null = null;
   private _disassemble: CallableFunction | null = null;
   private _cleanup: CallableFunction | null = null;
   private _allocUint8: CallableFunction | null = null;
-  private wasmFilePath: string;
-  private loaded: bool = false;
+  //private wasmFilePath: string;
+  private loaded: Boolean = false;
 
   constructor() {
-    this.wasmFilePath = "/home/seongmin/hama/zig-out/bin/hama.wasm";
+    //this.wasmFilePath = Bun.file("./hama.wasm");
   }
 
   async load(): Promise<void> {
     try {
-      if (WebAssembly.instantiate) {
-        const wasmFile = await Bun.file(
-          "/home/seongmin/hama/zig-out/bin/hama.wasm",
-        ).arrayBuffer();
-        const { instance } = await WebAssembly.instantiate(
-          wasmFile,
+      let wasmBuffer;
+      let wasmModule;
+
+      const wasmURL = new URL("./hama.wasm", import.meta.url);
+      if (typeof window === "undefined") {
+        // Node.js
+        const require = createRequire(import.meta.url);
+        const fs = require("fs");
+        wasmBuffer = fs.readFileSync(wasmURL);
+        wasmModule = await WebAssembly.instantiate(wasmBuffer, importObject);
+      } else {
+        // Browser
+        const response = await fetch(wasmURL);
+        wasmBuffer = await response.arrayBuffer();
+        wasmModule = await WebAssembly.instantiateStreaming(
+          wasmBuffer,
           importObject,
         );
-        this.wasmInstance = instance;
-        this._disassemble = this.wasmInstance.exports
-          .disassemble as CallableFunction;
-        this._cleanup = this.wasmInstance.exports.cleanup as CallableFunction;
-        this._allocUint8 = this.wasmInstance.exports
-          .allocUint8 as CallableFunction;
-        this.memory = this.wasmInstance.exports.memory as WebAssembly.Memory;
-        this.loaded = true;
       }
+      //const wasmModule = await WebAssembly.instantiateStreaming(wasmBuffer, importObject);
+      this.wasmInstance = wasmModule.instance;
+      this._disassemble = this.wasmInstance.exports
+        .disassemble as CallableFunction;
+      this._cleanup = this.wasmInstance.exports.cleanup as CallableFunction;
+      this._allocUint8 = this.wasmInstance.exports
+        .allocUint8 as CallableFunction;
+      this.memory = this.wasmInstance.exports.memory as WebAssembly.Memory;
+      this.loaded = true;
     } catch (error) {
       console.error("Error loading WASM module:", error);
     }
   }
 
   encodeString(string): Uint8Array {
-    const buffer = new TextEncoder().encode(string);
-    const pointer = this._allocUint8(buffer.length + 1); // ask Zig to allocate memory
-    const slice = new Uint8Array(
-      this.memory.buffer, // memory exported from Zig
-      pointer,
-      buffer.length + 1,
-    );
-    slice.set(buffer);
-    slice[buffer.length] = 0; // null byte to null-terminate the string
-    return pointer;
+    if (this.loaded) {
+      const buffer = new TextEncoder().encode(string);
+      const pointer = this._allocUint8(buffer.length + 1); // ask Zig to allocate memory
+      const slice = new Uint8Array(
+        this.memory.buffer, // memory exported from Zig
+        pointer,
+        buffer.length + 1,
+      );
+      slice.set(buffer);
+      slice[buffer.length] = 0; // null byte to null-terminate the string
+      return pointer;
+    }
+
+    return new Uint8Array([]);
   }
 
   disassemble(text: string): string {
@@ -128,15 +144,3 @@ class JamoParser {
 }
 
 export { JamoParser };
-
-// Example of how to use the JamoParser class
-async function main() {
-  const j = new JamoParser();
-  await j.load();
-  const string = "힘 내라 힘!";
-  const pointer = j.disassemble(string);
-  console.log("Final", pointer);
-}
-
-// Call the main function to execute the example
-main().catch(console.error);
